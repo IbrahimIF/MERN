a078613a125035111cec6b1d14a805a00d7c5023
Ibrahim Farrah <ibrahimfarrah30@gmail.com>
2025-04-02 15:48:29 +0100

    Updated backend CORS settings

--- api/server.js
+++ api/server.js
@@ -15,7 +15,10 @@ app.use(cors({
     'http://localhost:5173', //localhost
     'https://mern-frontend-kappa-drab.vercel.app/', //vercel link
     'https://mern-i8eg.onrender.com/' //render.com link
-  ]
+  ],
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type'],
+  credentials: true
 }));
 app.use(express.json());
 
